09d940c7ec843dc04dcd333f7c361c4157603ef5
alan1998 <alan_drake@btinternet.com>
2016-11-22 22:11:50 +0000

    Add project files.

---
+++ OpenCV_Balls1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenCV_Balls1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog.ShowDialog(this) != DialogResult.Cancel)
+            {
+                Bitmap img = new Bitmap(openFileDialog.FileName);
+                this.pictBox.Image = img;
+            }
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            Size sz = this.ClientSize;
+            pictBox.ClientSize = sz;
+        }
+
+
+    }
+}
